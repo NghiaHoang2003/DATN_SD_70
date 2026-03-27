24ae2d3c00ce7799bf97ab0387d136aa109dfc82
Sherry-334 <hoangtuananhstay@gmail.com>
2026-03-27 18:16:49 +0700

    giao diện

---
+++ DATN_70/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_70.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Bắt đường dẫn mặc định "/"
+        public IActionResult Index()
+        {
+            // Tự động tìm và trả về file Views/Home/Index.cshtml
+            return View();
+        }
+        public IActionResult Details()
+        {
+            // Chỉ định đích danh file Index trong thư mục ChiTietSanPham
+            return View("~/Views/ChiTietSanPham/Index.cshtml");
+        }
+        public IActionResult Cart()
+        {           
+            return View("~/Views/GioHang/Index.cshtml");
+        }
+        public IActionResult Checkout()
+        {           
+            return View("~/Views/PhuongThucThanhToan/Index.cshtml");
+        }
+
+
+    }
+}
--- DATN_70/Program.cs
+++ DATN_70/Program.cs
@@ -6,8 +6,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<DATN_70.Data.SqlConnectionFactory>();
 builder.Services.AddScoped<DATN_70.Services.IStoreRepository, DATN_70.Services.StoreRepository>();
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.UseSwagger();
 app.UseSwaggerUI();
 
